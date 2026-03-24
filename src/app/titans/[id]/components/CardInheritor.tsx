975c060c2bb655336e6ff80ade97ecabb68ae4d1
Pablo <padoro89@gmail.com>
2026-03-24 20:31:23 +0100

    fix errors href image

--- src/app/titans/[id]/components/CardInheritor.tsx
+++ src/app/titans/[id]/components/CardInheritor.tsx
@@ -1,7 +1,5 @@
 import { useGetCharacter } from "@/app/characters/[id]/hooks/useGetCharacter";
-import { AppRoutes } from "@/AppRoutes";
 import Image from "next/image";
-import Link from "next/link";
 
 interface CardFormerInheritorProps {
   inheritor: string;
@@ -12,19 +10,18 @@ export default function CardInheritor({ inheritor }: CardFormerInheritorProps) {
   const { data } = useGetCharacter(id);
 
   return (
-    <Link
-      href={`${AppRoutes.characters}/${id}`}
-      className="group cursor-pointer"
-    >
+    <div className="group cursor-pointer">
       <div className="relative aspect-3/4 mb-6 overflow-hidden bg-surface-container-high border-b-2 border-transparent group-hover:border-primary transition-all">
-        <Image
-          className="w-full h-full object-cover group-hover:scale-105 transition-all duration-700"
-          alt="Portrait of a young man with intense eyes and long hair"
-          src={data?.img ?? ""}
-          width={300}
-          height={400}
-          priority
-        />
+        {data && (
+          <Image
+            className="w-full h-full object-cover group-hover:scale-105 transition-all duration-700"
+            alt="Portrait of a young man with intense eyes and long hair"
+            src={data.img ?? ""}
+            width={300}
+            height={400}
+            priority
+          />
+        )}
 
         <div
           className={`absolute top-4 right-4 ${data?.status === "Alive" ? "bg-primary-container" : "bg-tertiary-container"} backdrop-blur-sm px-3 py-1 text-[10px] font-label text-on-primary-container tracking-widest uppercase`}
@@ -36,6 +33,6 @@ export default function CardInheritor({ inheritor }: CardFormerInheritorProps) {
       <h4 className="font-headline text-2xl text-on-surface group-hover:text-primary transition-colors">
         {data?.name}
       </h4>
-    </Link>
+    </div>
   );
 }
